592acd18a3213c0c0e46fb711fd295e5f7344b92
jason-deng <jasondeng.dev@gmail.com>
2026-08-12 19:09:40 +0800

    chore: add implementation structure to orderbook.cpp

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,2 +1,13 @@
 #include "../include/orderbook.h"
 #include "../include/trader.h"
+
+bool Orderbook::buy(Trader *trader, int quantity, int price) {
+  int balance = trader->getBalance();
+  if (balance < quantity*price) return false;
+  
+}
+
+bool Orderbook::sell(Trader *trader, int quantity, int price) {}
+
+bool Orderbook::cancelBuy(Trader *trader, int quantity, int price) {}
+bool Orderbook::cancelSell(Trader *trader, int quantity, int price) {}
